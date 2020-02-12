a97ff0bc36eaad05de954498aebf0bc3eaeddc67
Clavain <clavainova@gmail.com>
2020-02-12 11:29:14 +0100

    small css fixes

--- components/panier.php
+++ components/panier.php
@@ -11,6 +11,8 @@
 
     require "classes/basket.php";
     require "classes/produit.php";
+    $totalPrice = 0; //to keep track of total price
+    $totalQuantity = 0; //to track the total quantity
     $basket = new Basket();
     $items = $basket->getBasket();
     // var_dump($_COOKIE["basket"]);
@@ -33,8 +35,11 @@
                     <h1><?php print($product->__get("name")); ?>"</h1>
                     <p>PPU: <br>
                         Quantité: <?php print($item[1]); //quantity in cookie not db 
+                                    $totalQuantity += $item[1]; //increment total quantity
                                     ?><br>
-                        Prix: <?php print($product->__get("price")); ?>"</p>
+                        Prix: <?php print($product->__get("price") . "€");
+                                $totalPrice += ($product->__get("price") * $item[1]); //increment total with price*quantity 
+                                ?></p>
                     <form action="basket_management/removeFromBasket.php" method="POST">
                         <input style="display:none;" type="text" id="id" name="id" value="<?php print($item[0]); ?>" />
                         <input style="display:none;" type="text" id="quantity" name="quantity" value="<?php print($item[1]); ?>" />
@@ -51,10 +56,12 @@
 <div class="highlightbox">
     <h3>Postage:</h3>
     <p>0€00</p>
+    <h3>Quantity:</h3>
+    <?php print($totalQuantity); ?>
     <h3>Items:</h3>
-    <p>10€00</p>
+    <p><?php print($totalPrice . ".00€"); ?></p>
     <h3 class="underline">Total:</h3>
-    <p class="underline">10€00</p>
+    <p class="underline"></p>
     <button>→</button>
 </div>
 
